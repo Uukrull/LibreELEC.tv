7c5df415880ab28d8ae120f70f3af1926169a885
afl1 <afl2001@gmail.com>
2017-11-06 15:05:39 +0100

    dvb_tv-aml: switch to afl1
    media_build: revert to 2017-04-17-rpi
    
    tvheadend42: switch to 4.3
    
    package.mk: remove PKG_IS_ADDON="no"
    
    tvheadend42: bump to 4.3-628
    
    webgrabplus: bump to v2.1.5

--- packages/addons/service/tvheadend42/package.mk
+++ packages/addons/service/tvheadend42/package.mk
@@ -1,5 +1,5 @@
 ################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
+#      This file is part of LibreELEC - https://LibreELEC.tv
 #      Copyright (C) 2016-present Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
@@ -17,10 +17,10 @@
 ################################################################################
 
 PKG_NAME="tvheadend42"
-PKG_VERSION="407c8a3"
-PKG_SHA256="8569ab42650ca02e47d3d88276342fe2ffde4f40bd11893a3bf6879a79f36fb3"
-PKG_VERSION_NUMBER="4.2.3-20"
-PKG_REV="112"
+PKG_VERSION="10e6dbe9"
+PKG_SHA256="ed8860a6792f2c075f6ef4504e997f353a80d4c3d75ecd1fc212eda980ab8a56"
+PKG_VERSION_NUMBER="4.3-628"
+PKG_REV="43-628"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.tvheadend.org"
@@ -33,7 +33,7 @@ PKG_LONGDESC="Tvheadend ($PKG_VERSION_NUMBER): is a TV streaming server for Linu
 PKG_AUTORECONF="no"
 
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="Tvheadend Server 4.2"
+PKG_ADDON_NAME="Tvheadend Server 4.3"
 PKG_ADDON_TYPE="xbmc.service"
 
 # transcoding only for generic
